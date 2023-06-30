28e212caa810e4273ed8f186a2672282e06a87ae
Sendeky <spruslasha@gmail.com>
2023-06-29 23:39:25 -0700

    initial commit, setup OpenCV Headers/Lib

---
+++ VideoDetectLanes.cpp/VideoDetectLanes.cpp/main.cpp
@@ -0,0 +1,16 @@
+//
+//  main.cpp
+//  VideoDetectLanes.cpp
+//
+//  Created by Ruslan Spirkin on 6/29/23.
+//
+
+#include <iostream>
+#include </opt/homebrew/Cellar/opencv/4.8.0/include/opencv2/opencv.hpp>         // path is a bit different because M1 Mac Homebrew install (I think?)
+
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
