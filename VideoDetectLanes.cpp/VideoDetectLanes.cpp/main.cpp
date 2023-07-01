27cd18201374ab0609ee3387dcba0c3fe3b68037
Sendeky <spruslasha@gmail.com>
2023-06-30 23:56:12 -0700

    working on getting opencv to read video...

--- VideoDetectLanes.cpp/VideoDetectLanes.cpp/main.cpp
+++ VideoDetectLanes.cpp/VideoDetectLanes.cpp/main.cpp
@@ -12,5 +12,27 @@
 int main(int argc, const char * argv[]) {
     // insert code here...
     std::cout << "Hello, World!\n";
+    
+    // get path to video
+    std::string videoPath;
+    std::cout << "Enter Video Path:";
+    std::cin >> videoPath;
+    
+    std::cout << "videoPath: " << videoPath;
+    
+    cv::VideoCapture cap;
+    
+    if (videoPath != "") {
+        std::cout << "Yay!";
+        cap.open(videoPath);
+    }
+    
+    // if no success, exit program
+    if (cap.isOpened() == false) {
+        std::cout << "Cannot open video file";
+        std::cin.get(); //wait for any key press
+        return -1;
+    }
+    
     return 0;
 }
