5450ba1b8895e45180f0ef4ab0f033aa68a4bd6f
Sendeky <spruslasha@gmail.com>
2023-07-05 21:44:06 -0700

    added code to display the video

--- VideoDetectLanes.cpp/VideoDetectLanes.cpp/main.cpp
+++ VideoDetectLanes.cpp/VideoDetectLanes.cpp/main.cpp
@@ -34,5 +34,40 @@ int main(int argc, const char * argv[]) {
         return -1;
     }
     
+    //Uncomment for skipping to video position
+    //cap.set(CAP_PROP_POS_MSEC, 300);
+    
+    // gets fps of video
+    double fps = cap.get(cv::CAP_PROP_FPS);
+    std::cout << "Video fps: " << fps << std::endl;
+    
+    // OpenCV window name
+    cv::String window_name = "OpenCV WIndow";
+    
+    // creates window
+    cv::namedWindow(window_name, cv::WINDOW_NORMAL);
+    
+    while (true) {
+        cv::Mat frame;
+        bool bSuccess = cap.read(frame);    // read new frame from video
+        
+        // breaks the while loop at the end of the video or if reading frame is false
+        if (bSuccess == false) {
+            std::cout << "Found the end of the video" << std::endl;
+            break;
+        }
+        
+        // show frame in created window
+        cv::imshow(window_name, frame);
+
+        //wait for for 10 ms until any key is pressed.
+        //If the 'Esc' key is pressed, break the while loop.
+        //If the any other key is pressed, continue the loop
+        //If any key is not pressed withing 10 ms, continue the loop
+        if (cv::waitKey(5) == 27) {
+            std::cout << "Esc key is pressed by user. Stoppig the video" << std::endl;
+            break;
+        }
+    }
     return 0;
 }
